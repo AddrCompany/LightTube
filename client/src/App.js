386efa10c98f136424538ada97f6d2c0e98a9d19
uneeb123 <uneeb.i.am@gmail.com>
2019-01-11 20:23:11 -0500

    SideNav appears

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import SideNav, { NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import { Route } from 'react-router-dom';
+import Home from './Components/Home';
+import Upload from './Components/Upload';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Route render={({ location, history }) => (
+          <React.Fragment>
+              <SideNav
+                  onSelect={(selected) => {
+                      const to = '/' + selected;
+                      if (location.pathname !== to) {
+                          history.push(to);
+                      }
+                  }}
+                  style={{background: '#919191', borderRight: '1px solid'}}
+              >
+                  <SideNav.Toggle />
+                  <SideNav.Nav defaultSelected="home">
+                      <NavItem eventKey="">
+                          <NavIcon>
+                              <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                          </NavIcon>
+                          <NavText>
+                              Home
+                          </NavText>
+                      </NavItem>
+                      <NavItem eventKey="upload">
+                          <NavIcon>
+                            <i className="fa fa-fw fa-upload" style={{ fontSize: '1.75em' }} />
+                          </NavIcon>
+                          <NavText>
+                              Devices
+                          </NavText>
+                      </NavItem>
+                  </SideNav.Nav>
+              </SideNav>
+              <main>
+                  <Route path="/" exact component={props => <Home />} />
+                  <Route path="/upload" component={props => <Upload />} />
+              </main>
+          </React.Fragment>
+      )}
+      />
+    </div>
     );
   }
 }
---
+++ client/src/Components/Upload.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Upload extends Component {
+    render() {
+      return (
+        <header className="App-header">
+          <p>
+            Upload
+          </p>
+        </header>
+      );
+    }
+  }
