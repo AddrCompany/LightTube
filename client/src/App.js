924dbec7b36f93fcae0d0c076abf393547d2abb4
uneeb123 <uneeb.i.am@gmail.com>
2019-01-11 23:53:26 -0500

    Fix the toggle sideNav issue

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,11 @@ import Upload from './Components/Upload';
 import './App.css';
 
 class App extends Component {
+  state = {
+    atHome: true,
+    atUpload: false,
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,16 +19,23 @@ class App extends Component {
           <React.Fragment>
               <SideNav
                   onSelect={(selected) => {
-                      const to = '/' + selected;
-                      if (location.pathname !== to) {
-                          history.push(to);
-                      }
+                    let to = '/'
+                    if (selected === "") {
+                      this.setState({atHome: true, atUpload: false});
+                    }
+                    else if (selected === "upload") {
+                      to = '/' + selected;
+                      this.setState({atHome: false, atUpload: true});
+                    }
+                    if (location.pathname !== to) {
+                      history.push(to);
+                    }
                   }}
                   style={{background: '#919191', borderRight: '1px solid'}}
               >
                   <SideNav.Toggle />
                   <SideNav.Nav defaultSelected="home">
-                      <NavItem eventKey="">
+                      <NavItem eventKey="" active={this.state.atHome}>
                           <NavIcon>
                               <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
                           </NavIcon>
@@ -31,12 +43,12 @@ class App extends Component {
                               Home
                           </NavText>
                       </NavItem>
-                      <NavItem eventKey="upload">
+                      <NavItem eventKey="upload" active={this.state.atUpload}>
                           <NavIcon>
                             <i className="fa fa-fw fa-upload" style={{ fontSize: '1.75em' }} />
                           </NavIcon>
                           <NavText>
-                              Devices
+                              Upload
                           </NavText>
                       </NavItem>
                   </SideNav.Nav>
