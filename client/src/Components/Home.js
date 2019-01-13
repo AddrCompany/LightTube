cbb562163e70f43ea65c9c54070a5049a1d16d88
uneeb123 <uneeb.i.am@gmail.com>
2019-01-12 21:39:07 -0500

    Infinite Scroll

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,77 +1,71 @@
 import React, { Component } from 'react';
+import InfiniteScroll from 'react-infinite-scroller';
 
 import './Home.css';
 
 export default class Home extends Component {
-    render() {
-      return (
-        <div className="Videos-full">
-          <header className="Videos-header">
-            <p style={{color: 'white', fontFamily: 'Sans Serif'}}>New Videos</p>
-          </header>
-          <div className="Videos-all container-fluid">
-            <div className="row">
-              <div className="col-3">
-                <div className="Video-container" />
-              </div>
-              <div className="col-3">
-                <div className="Video-container" />
-              </div>
-              <div className="col-3">
-                <div className="Video-container" />
-              </div>
-              <div className="col-3">
-                <div className="Video-container" />
-              </div>
-            </div>
+  // https://github.com/CassetteRocks/react-infinite-scroller/blob/master/docs/src/index.js
+  state = {
+    hasMoreItems: true,
+    total: 16,
+  };
 
-            <div className="row">
-              <div className="col-3">
-                <div className="Video-container" />
-              </div>
-              <div className="col-3">
-                <div className="Video-container" />
-              </div>
-              <div className="col-3">
-                <div className="Video-container" />
-              </div>
-              <div className="col-3">
-                <div className="Video-container" />
-              </div>
-            </div>
+  loadItems = (page) => {
+    let self = this;
+    setTimeout(function() {
+      let next = self.state.total + 12;
+      self.setState({total: next});
+    }, 1000);
+  }
 
-            <div className="row">
-              <div className="col-3">
-                <div className="Video-container" />
-              </div>
-              <div className="col-3">
-                <div className="Video-container" />
-              </div>
-              <div className="col-3">
-                <div className="Video-container" />
-              </div>
-              <div className="col-3">
-                <div className="Video-container" />
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="col-3">
-                <div className="Video-container" />
-              </div>
-              <div className="col-3">
-                <div className="Video-container" />
-              </div>
-              <div className="col-3">
-                <div className="Video-container" />
-              </div>
-              <div className="col-3">
-                <div className="Video-container" />
-              </div>
-            </div>
-
+  currentRow = (start) => {
+    let end = start + 3;
+    let allItems = [];
+    for (let i = start; i <= Math.min(end, this.state.total); i++) {
+      allItems.push(
+          <div className="col-3" key={i}>
+            <div className="Video-container" />
           </div>
-        </div>
       );
     }
+    return allItems;
   }
+
+  render() {
+    const loader = <div key={this.state.total} className="loader">Loading ...</div>;
+    let items = [];
+    let i = 0;
+    let rowIndex = 0;
+    let totalRows = Math.ceil(this.state.total/4.0);
+    while (rowIndex < totalRows) {
+      let row = this.currentRow(i);
+      items.push(
+        <div className="row" key={'row'+rowIndex}>
+          {row}
+        </div>
+      );
+      i += 4;
+      rowIndex += 1;
+    }
+
+    return (
+      <div className="Videos-full">
+        <header className="Videos-header">
+          <p style={{color: 'white', fontFamily: 'Sans Serif'}}>New Videos</p>
+        </header>
+        <div className="Videos-all container-fluid">
+          <InfiniteScroll
+            pageStart={0}
+            loadMore={this.loadItems}
+            hasMore={this.state.hasMoreItems}
+            loader={loader}>
+
+            <div>
+              {items}
+            </div>
+          </InfiniteScroll>
+        </div>
+      </div>
+    );
+  }
+}
