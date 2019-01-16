b83724b034487e1746413ea5809f9473c761d2e0
uneeb123 <uneeb.i.am@gmail.com>
2019-01-16 15:24:58 -0500

    Updates to models/Added Metadata

--- server/src/model.ts
+++ server/src/model.ts
@@ -5,56 +5,81 @@ const videoDataTypes = {
     title: { type: sequelize.STRING, allowNull: false },
     user: { type: sequelize.STRING, defaultValue: "Anonymous" },
     description: { type: sequelize.TEXT, defaultValue: "No description" },
-    uploaded_at: { type: sequelize.TIME },
     likes: { type: sequelize.INTEGER, defaultValue: 0 },
     dislikes: { type: sequelize.INTEGER, defaultValue: 0 },
     views: { type: sequelize.INTEGER, defaultValue: 0 },
-    transcoder_guid: { type: sequelize.UUID }
+    ready: { type: sequelize.BOOLEAN,  defaultValue: false },
 };
 
 const commentDataTypes = {
     id: { type: sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-    video_id: { type: sequelize.INTEGER },
-    comment: { type: sequelize.TEXT, allowNull: false},
-    user: { type: sequelize.STRING, defaultValue: "Anonymous"}
+    video_id: { type: sequelize.INTEGER, allowNull: false },
+    comment: { type: sequelize.TEXT, allowNull: false },
+    user: { type: sequelize.STRING, defaultValue: "Anonymous" }
+}
+
+const videoMetadataDataTypes = {
+    id: { type: sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+    video_id: { type: sequelize.INTEGER, allowNull: false },
+    local_file_name: { type: sequelize.STRING, allowNull: false },
+    transcoder_guid: { type: sequelize.UUID }, // also means S3 upload done/local file cleaned up
+    img_url: { type: sequelize.STRING },
+    cloudfront_dash_url: { type: sequelize.STRING }
 }
 
 export interface VideoAttrs {
-    id: number,
+    id?: number,
     title: string,
-    user: string,
-    description: string,
-    uploaded_at: Date,
-    likes: number,
-    dislikes: number,
-    views: number,
-    transcoder_guid: string
+    user?: string,
+    description?: string,
+    likes?: number,
+    dislikes?: number,
+    views?: number,
+    ready?: boolean,
+    comments?: [ CommentAttrs ]
 }
 
 export interface CommentAttrs {
-    id: number,
+    id?: number,
     video_id: number,
     comment: string,
-    user: string
+    user?: string,
+    video?: VideoAttrs
+}
+
+export interface VideoMetadataAttrs {
+    id?: number,
+    video_id: number,
+    local_file_name: string,
+    transcoder_guid?: string,
+    img_url?: string,
+    cloudfront_dash_url?: string,
+    video?: VideoAttrs
 }
 
 export interface Video extends sequelize.Instance<VideoAttrs> {}
 export interface Videos extends sequelize.Model<Video, VideoAttrs> {}
 export interface Comment extends sequelize.Instance<CommentAttrs> {}
 export interface Comments extends sequelize.Model<Comment, CommentAttrs> {}
+export interface VideoMetadata extends sequelize.Instance<VideoMetadataAttrs> {}
+export interface VideosMetadata extends sequelize.Model<VideoMetadata, VideoMetadataAttrs> {}
 
 export interface Models extends sequelize.Models {
     videos: Videos,
-    comments: Comments
+    comments: Comments,
+    videosMetadata: VideosMetadata
 }
 
 export function instantiateModels(sequelizeInstance: sequelize.Sequelize): Models {
     const videos = sequelizeInstance.define<Video, VideoAttrs>('videos', videoDataTypes);
     const comments = sequelizeInstance.define<Comment, CommentAttrs>('comments', commentDataTypes);
-    // videos.hasMany(comments, { foreignKey: 'video_id' })
+    const videosMetadata = sequelizeInstance.define<VideoMetadata, VideoMetadataAttrs>('metadata', videoMetadataDataTypes);
+    videosMetadata.hasOne(videos, { foreignKey: 'video_id', as: 'video' });
+    videos.hasMany(comments, { foreignKey: 'video_id', as: 'comments' });
     comments.belongsTo(videos, { foreignKey: 'video_id', as: 'video'} );
     return {
         videos: videos,
-        comments: comments
+        comments: comments,
+        videosMetadata: videosMetadata
     };
 }
--- server/src/router.ts
+++ server/src/router.ts
@@ -1,16 +1,24 @@
 import * as express from "express";
 import { UploadedFile } from "express-fileupload";
 import * as uuidv4 from 'uuid/v4';
+import * as Promise from 'bluebird';
 
-import { Models } from "./model";
+import { Models, VideoAttrs, Video, VideoMetadataAttrs } from "./model";
 
 const SUPPORTED_EXTENSIONS = ["mp4", "mpg", "m4v", "m2ts", "mov"];
 
 export interface ServerRequest extends express.Request {
-    models?: Models,
-    body: any
+    models?: Models
 }
 export interface ServerResponse extends express.Response {}
+
+interface UploadRequest extends ServerRequest {
+    body: {
+        title: string,
+        description: string,
+        user: string
+    }
+}
 
 const router = express.Router();
 
@@ -22,7 +30,7 @@ router.get('/all', function(req: ServerRequest, res: ServerResponse) {
     // database call and return
 });
 
-router.post('/upload', function(req: ServerRequest, res: ServerResponse) {
+router.post('/upload', function(req: UploadRequest, res: ServerResponse) {
     const videoFile: UploadedFile = req.files.file as UploadedFile;
     const videoFileExtension = videoFile.name.split('.').pop();
     if (SUPPORTED_EXTENSIONS.indexOf(videoFileExtension) === -1) {
@@ -35,11 +43,35 @@ router.post('/upload', function(req: ServerRequest, res: ServerResponse) {
             if (err) {
                 return res.status(500).send(err)
             }
-            res.json({
-                fileName: videoFileName,
-            })
+            const videoParams: VideoAttrs = {
+                title: req.body.title,
+                description: req.body.description,
+                user: req.body.user
+            };
+            persistNewVideo(videoParams, req.models, videoFileName)
+            .then(video =>
+                res.json({
+                    title: video.get().title,
+                    description: video.get().description,
+                    user: video.get().user
+                })
+            );
         },
     );
 });
+
+function persistNewVideo(
+    videoParams: VideoAttrs, models: Models, fileName: string
+): Promise<Video> {
+    return models.videos.create(videoParams)
+    .then(video => {
+        const metadata: VideoMetadataAttrs = {
+            video_id: video.get().id,
+            local_file_name: fileName
+        }
+        return models.videosMetadata.create(metadata)
+        .then(() => video);
+    })   
+}
 
 export const mainRouter = router;
